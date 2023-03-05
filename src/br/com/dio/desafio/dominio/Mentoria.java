6fd664eb0f1dcac34adbeb05b02ca86ff11eb8c7
Francesly Hebe <f.francesly@gmail.com>
2023-03-04 22:23:09 -0300

    Iniciando atividade

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,31 +3,8 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 
 public class Mentoria extends Conteudo{
+    String titulo;
+    String descricao;
+    LocalDate data;
 
-    private LocalDate data;
-
-    @Override
-    public double calcularXp() {
-        return XP_PADRAO + 20d;
-    }
-
-    public Mentoria() {
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
-    }
 }
