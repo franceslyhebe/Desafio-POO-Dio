91e6acc1091bff7be3bde20180967df11946bef5
Francesly Hebe <f.francesly@gmail.com>
2023-03-05 00:23:02 -0300

    Finalizando

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,33 @@ public class Main {
         Mentoria mentoria = new Mentoria();
         mentoria.setMentoria("Mentoria de Java", "Se prepare para o mercado de trabalho!", LocalDate.now());
 
-        System.out.println(cursoJava);
-        System.out.println(cursoReact);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setBootcamp("Java Developer", "Bootcamp para se especializar em Java");
+        bootcamp.getConteudos().add(cursoJava);
+        bootcamp.getConteudos().add(mentoria);
+
+        Bootcamp bootcamp2 = new Bootcamp();
+        bootcamp2.setBootcamp("React Developer", "Bootcamp para se especializar em React");
+        bootcamp2.getConteudos().add(cursoReact);
+        bootcamp2.getConteudos().add(mentoria);
+
+        Dev developer = new Dev();
+        developer.setNome("Francesly");
+        developer.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + developer.getConteudoInscritos());
+        developer.progredir();
+        System.out.println("Conteudos Concluídos" + developer.getConteudosConcluidos());
+
+
+
+        Dev developer2 = new Dev();
+        developer2.setNome("Camila");
+        developer2 .inscreverBootcamp(bootcamp2);
+        System.out.println("Conteudos Inscritos" + developer2.getConteudoInscritos());
+        System.out.println("Conteudos Concluídos" + developer2.getConteudosConcluidos());
+
+
+
     }
 
 }
